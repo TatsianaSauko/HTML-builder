01f64750fae086417250580da5fda3370064e77c
TatsianaSauko <miljanzowa@mail.ru>
2023-05-04 12:02:01 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+let folderPath = path.join(__dirname, 'files');
+let folderCopy = path.join(__dirname, 'files-copy');
+const copyFiles = (files) => {
+  files.forEach(file => {
+    fs.copyFile(path.join(folderPath, file.name), path.join(folderCopy, file.name), (err) => {
+      if (err) throw err;
+    });
+  });
+};
+const creatFolder = (files) => {
+  fs.mkdir(folderCopy, { recursive: true }, err => {
+    if(err) throw err;
+    copyFiles(files);
+  });
+};
+const readFolder = () => {
+  fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    creatFolder(files);
 
+  });
+};
+const checkFolderCreating = () => {
+  fs.stat(folderCopy, (err) => {
+    if (!err) {
+      fs.rm(folderCopy, { recursive: true },err => {
+        if(err) throw err;
+        readFolder();
+      });
+    }
+    else if (err.code === 'ENOENT') {
+      readFolder();
+    }
+  });
+};
+
+const copyDir = () => {
+  checkFolderCreating();
+};
+
+copyDir();
